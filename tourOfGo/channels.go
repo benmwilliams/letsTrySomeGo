ff0fa57cda998f8eec4a295db20fbb04a7c8190a
Ben Williams <benwilliamsdev@gmail.com>
2016-10-14 00:44:55 -0400

    made a change in hello.go to print a new line

--- tourOfGo/channels.go
+++ tourOfGo/channels.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
+func sum(s []int, c chan int) { // sum() takes an array of ints and an int channel
+	sum := 0              // var sum int = 0
+	for _, v := range s { // ?????
+		sum += v // sum = sum + v
 	}
 	c <- sum // send sum to c
 }
 
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
+	s := []int{7, 2, 8, -9, 4, 0} // var s int = [7, 2, 8, -9, 4, 0]
 
-	c := make(chan int)
-	go sum(s[:len(s)/2], c)
+	c := make(chan int)     // make???
+	go sum(s[:len(s)/2], c) // run sum(), passing
 	go sum(s[len(s)/2:], c)
 	x, y := <-c, <-c // receive from c
 
