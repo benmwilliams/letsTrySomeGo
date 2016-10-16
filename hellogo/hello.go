aa6e100b1ea21ea5082d328b6325bbe9830e72f2
Ben Williams <benwilliamsdev@gmail.com>
2016-10-16 17:09:42 -0400

    I dont remember

--- hellogo/hello.go
+++ hellogo/hello.go
@@ -1,4 +1,4 @@
-// TODO: moved this file, changed the function of the program to ask for a name and then greet.
+// NOTE: moved this file, changed the function of the program to ask for a name and then greet.
 // the original binary is still in go/bin, as well as the newly created hellogo.
 // this shows that the binary is created based on the dir name, and not the name of the source file
 // also there is a binary in this directory, and Im not sure if it's a result of a bad config of an errant build step
@@ -6,11 +6,19 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/benmwilliams/letsTrySomeGo/readline"
+)
 
 func main() {
 	fmt.Printf("What is your name? \n")
-	var name string
-	fmt.Scanln(&name)
+
+	name := readline.ReadLine(os.Stdin, func(line string) {
+		return line
+	})
+
 	fmt.Printf("Hello, %v \n", name)
 }
