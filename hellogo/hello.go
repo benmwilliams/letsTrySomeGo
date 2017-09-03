478271214d3cb976749d1590e7f49384454f0e65
Ben Williams <benwilliamsdev@gmail.com>
2017-09-03 00:51:49 -0400

    case closed

--- hellogo/hello.go
+++ hellogo/hello.go
@@ -1,14 +1,18 @@
-// NOTE: I'm definitely retarded.  Exiting the program from greet() makes it work as expected
-// UNLESS: you hit enter without typing anything.  a single character routes through the first part of the if{}
-// > 1 char gives you the else, but no chars just returns without running greet.
-
-// NOTE: just moved this project from arch linux to windows10... this bug
-// no longer takes effect, and the program seems to work as expected. Interesting.
-
-// NOTE: Just tried on Ubuntu. Same bug as on arch. What is the meaning of this? Guess I should try on OSX too. Something about readline pkg doesn't work right, I guess.
-
-// NOTE: never forget to set git user.email and user.name
-// you want this shit to count. How else are people supposed to know that you're doing anything?
+// What I learned: this silly little project frustrated me for a long time.
+// For really no reason other than not understanding why, I wanted to be able
+// to have an enter keypress just log and empty response and close out the
+// program. It never worked on linux, but then when I tried on windows, it
+// worked perfectly. Now the mystery is why it works on windows, and the truth
+// is that is was working on linux all the time. The readline package trims the
+// newline character from the input, so in the console you get a new line, but
+// the program doesn't terminate. Once I removed the line that trimmed `\n`,
+// pressing enter would terminate the program with the greeting adressed to
+// "newline binary character". I now realize this is the only reasonable
+// behavior. My frustration was not from this code not doing what I wanted,
+// but from my wanting something illogical. I don't think I even need the
+// readline package to do this, but I'm ready to close this and move on to
+// something else. This should really just be like 15 lines of code.
+// buuuut... why *does* it work the way I wanted on windows?
 
 package main
 
@@ -25,8 +29,8 @@ func main() {
 
 	readline.ReadLine(os.Stdin, func(line string) {
 		r := []rune(line)
-		if len(r) <= 1 || line == "" {
-			greet(string(len(r))) // this is only reached if len(r) == 1, not if len(r) < 1.
+		if len(r) < 1 || line == "\n" {
+			greet(string(len(r))) // this would never exec bc readline trims \n
 		} else {
 			name = line
 			greet(name)
