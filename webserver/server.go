b863c87377beb99336860f6fd84e175b1527a1ba
Ben Williams <benwilliamsdev@gmail.com>
2018-01-10 21:24:08 -0500

    serving hello world

--- webserver/server.go
+++ webserver/server.go
@@ -1,1 +1,16 @@
 // a webserver in go
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8888", nil)
+}
