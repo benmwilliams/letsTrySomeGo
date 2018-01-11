c30da74965f293a2331ccd2069dff33845f8423d
Ben Williams <benwilliamsdev@gmail.com>
2018-01-10 22:52:47 -0500

    some useful comments for future me

--- webserver/templateHome.go
+++ webserver/templateHome.go
@@ -18,6 +18,18 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	// wow. Go uses layout numbers for time formatting. Seems like
+	// a really stupid idea. I'm going to figure out how to fix this problem
+	// for this, right now and then immediately forget how I did it because
+	// it's stupid. I'm really curious what the reasoning behing this was.
+	// Maybe one day I'll be curious enough to actually research it. Maybe
+	// not.
+	// Note to future me:
+	// The official docs are not very helpful on this.
+	// See this SO article:
+	// https://stackoverflow.com/questions/25845172/parsing-date-string-in-golang
+	// and this go package:
+	// https://github.com/araddon/dateparse
 	now := time.Now()
 	indexVars := PageVariables{
 		Date: now.Format("01-01-2020"),
